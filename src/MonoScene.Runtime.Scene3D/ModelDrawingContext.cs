837f9d514b210c7a79b50289d40e81d966e4c114
Balanced Guy <vjlopezcarrillo@gmail.com>
2021-07-09 13:35:23 -0600

    Shadowmap effect

--- src/MonoScene.Runtime.Scene3D/ModelDrawingContext.cs
+++ src/MonoScene.Runtime.Scene3D/ModelDrawingContext.cs
@@ -30,7 +30,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
         private readonly Stack<_GraphicsState> _PreserveState = new Stack<_GraphicsState>();
 
-        private float _FieldOfView = MathHelper.PiOver4;
+        private float _FieldOfView = MathHelper.Pi / 3.0f;
         private float _NearPlane = 1f;
         
         private Matrix _View, _Projection;
